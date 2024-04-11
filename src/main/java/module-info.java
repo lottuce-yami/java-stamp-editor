a0164b94d49bbffdac360ffe992c9d250bef7ce7
Lotte <lottuce.yami@gmail.com>
2024-04-11 16:42:04 +0200

    Add exporting to PNG

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module moe.lottuce.stampeditor {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.swing;
 
     opens moe.lottuce.stampeditor to javafx.fxml;
     opens moe.lottuce.stampeditor.controllers to javafx.fxml;
--- src/main/java/moe/lottuce/stampeditor/controllers/MainController.java
+++ src/main/java/moe/lottuce/stampeditor/controllers/MainController.java
@@ -1,12 +1,22 @@
 package moe.lottuce.stampeditor.controllers;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.VPos;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.TextField;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
 import javafx.scene.text.TextAlignment;
+import javafx.stage.FileChooser;
+
+import javax.imageio.ImageIO;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class MainController {
     @FXML
@@ -37,5 +47,28 @@ public class MainController {
                 canvasSide / 2,
                 canvasSide / 2
         );
+    }
+
+    @FXML
+    protected void onExport(ActionEvent actionEvent) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Експорт...");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("PNG", "*.png")
+        );
+        fileChooser.setInitialFileName("stamp");
+
+        File exportFile = fileChooser.showSaveDialog(stampCanvas.getScene().getWindow());
+        if (exportFile != null) {
+            int canvasSize = (int) stampCanvas.getWidth();
+            WritableImage writableImage = new WritableImage(canvasSize, canvasSize);
+
+            SnapshotParameters params = new SnapshotParameters();
+            params.setFill(Color.TRANSPARENT);
+            stampCanvas.snapshot(params, writableImage);
+
+            RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
+            ImageIO.write(renderedImage, "PNG", exportFile);
+        }
     }
 }
