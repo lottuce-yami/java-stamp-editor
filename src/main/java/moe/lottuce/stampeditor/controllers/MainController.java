69b85277c7d5852b806b68439ddd5789705a3b8c
Lotte <lottuce.yami@gmail.com>
2024-04-20 15:13:37 +0200

    Create class for circular text

--- src/main/java/moe/lottuce/stampeditor/controllers/MainController.java
+++ src/main/java/moe/lottuce/stampeditor/controllers/MainController.java
@@ -9,20 +9,16 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.TextField;
 import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.scene.transform.Affine;
-import javafx.scene.transform.Rotate;
 import javafx.stage.FileChooser;
 import moe.lottuce.stampeditor.models.CircularFrame;
+import moe.lottuce.stampeditor.models.CircularText;
 import moe.lottuce.stampeditor.models.HorizontalText;
 
 import javax.imageio.ImageIO;
 import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class MainController {
     @FXML
@@ -52,8 +48,12 @@ public class MainController {
                 new CircularFrame(stampCanvas, 2.5, Color.NAVY, 150)
         };
 
-        HorizontalText[] texts = {
+        HorizontalText[] horizontalTexts = {
                 new HorizontalText(stampCanvas, primaryTextField.getText(), new Font("Times New Roman", 16), Color.NAVY)
+        };
+
+        CircularText[] circularTexts = {
+                new CircularText(stampCanvas, additionalTextField.getText(), new Font("Times New Roman", 12), Color.NAVY, 165, 185, 535, 5)
         };
 
         gc.clearRect(0, 0, stampCanvas.getWidth(), stampCanvas.getHeight());
@@ -62,12 +62,13 @@ public class MainController {
             frame.draw();
         }
 
-        for (HorizontalText text : texts) {
+        for (HorizontalText text : horizontalTexts) {
             text.draw();
         }
 
-        Font additionalFont = new Font("Times New Roman", 12);
-        drawCircularText(additionalTextField.getText(), additionalFont, Color.NAVY, 5, 165, 185, 535);
+        for (CircularText text : circularTexts) {
+            text.draw();
+        }
     }
 
     @FXML
@@ -91,82 +92,5 @@ public class MainController {
             RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
             ImageIO.write(renderedImage, "PNG", exportFile);
         }
-    }
-
-    protected void drawCircularText(String text, Font font, Paint paint, double tracking, double diameter, double startAngle, double endAngle) {
-        double radius = diameter / 2;
-        double circlePerimeter = 2 * Math.PI * radius;
-        double angleInPixels = circlePerimeter / 360;
-        double angleArea = Math.abs(startAngle - endAngle);
-
-        int letterCount = text.replace(" ", "").length();
-        int spaceCount = text.length() - letterCount;
-
-        Text sceneText = new Text();
-        sceneText.setFont(font);
-
-        sceneText.setText(" ");
-        double trackingInPixels = (sceneText.getLayoutBounds().getHeight() / font.getSize()) * tracking;
-
-        double[] symbolsWidth = new double[text.length()];
-        for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-
-            if (c == ' ') {
-                symbolsWidth[i-1] -= trackingInPixels;
-                continue;
-            }
-
-            sceneText.setText(String.valueOf(c));
-            symbolsWidth[i] = sceneText.getLayoutBounds().getWidth() + trackingInPixels;
-
-            if (i == text.length() - 1) {
-                symbolsWidth[i] -= trackingInPixels;
-            }
-        }
-
-        double textWidthInAngles = Arrays.stream(symbolsWidth).sum() / angleInPixels;
-        double emptyAreaInAngles = angleArea - textWidthInAngles;
-        double spaceWidthInAngles = emptyAreaInAngles / spaceCount;
-
-        gc.setFont(font);
-        gc.setFill(paint);
-
-        double currentAngle = startAngle;
-
-        for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-
-            if (c == ' ') {
-                currentAngle += spaceWidthInAngles;
-                continue;
-            }
-
-            double x = getPointAtAngle(radius, currentAngle)[0];
-            double y = getPointAtAngle(radius, currentAngle)[1];
-
-            Rotate r = new Rotate(currentAngle + 90, x, y);
-            gc.setTransform(r.getMxx(), r.getMyx(), r.getMxy(), r.getMyy(), r.getTx(), r.getTy());
-
-            gc.fillText(String.valueOf(c), x, y);
-            currentAngle += symbolsWidth[i] / angleInPixels;
-        }
-
-        gc.setFont(Font.getDefault());
-        gc.setFill(Color.BLACK);
-        gc.setTransform(new Affine());
-    }
-
-    protected double[] getPointAtAngle(double radius, double angle) {
-        double centerX = stampCanvas.getWidth() / 2;
-        double centerY = stampCanvas.getHeight() / 2;
-
-        double x = radius * Math.cos(Math.toRadians(angle));
-        double y = radius * Math.sin(Math.toRadians(angle));
-
-        x += centerX;
-        y += centerY;
-
-        return new double[]{x, y};
     }
 }
---
+++ src/main/java/moe/lottuce/stampeditor/models/CircularText.java
@@ -0,0 +1,138 @@
+package moe.lottuce.stampeditor.models;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.transform.Affine;
+import javafx.scene.transform.Rotate;
+
+import java.util.Arrays;
+
+public class CircularText extends Text {
+    private double diameter;
+
+    public double getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(double diameter) {
+        this.diameter = diameter;
+    }
+
+    private double startAngle;
+
+    public double getStartAngle() {
+        return startAngle;
+    }
+
+    public void setStartAngle(double startAngle) {
+        this.startAngle = startAngle;
+    }
+
+    private double endAngle;
+
+    public double getEndAngle() {
+        return endAngle;
+    }
+
+    public void setEndAngle(double endAngle) {
+        this.endAngle = endAngle;
+    }
+
+    private double tracking;
+
+    public double getTracking() {
+        return tracking;
+    }
+
+    public void setTracking(double tracking) {
+        this.tracking = tracking;
+    }
+
+    public CircularText(Canvas canvas, String text, Font font, Paint paint, double diameter, double startAngle, double endAngle, double tracking) {
+        super(canvas, text, font, paint);
+        setDiameter(diameter);
+        setStartAngle(startAngle);
+        setEndAngle(endAngle);
+        setTracking(tracking);
+    }
+
+    @Override
+    public void draw() {
+        double radius = diameter / 2;
+        double circlePerimeter = 2 * Math.PI * radius;
+        double angleInPixels = circlePerimeter / 360;
+        double angleArea = Math.abs(startAngle - endAngle);
+
+        int letterCount = text.replace(" ", "").length();
+        int spaceCount = text.length() - letterCount;
+
+        javafx.scene.text.Text sceneText = new javafx.scene.text.Text();
+        sceneText.setFont(font);
+
+        sceneText.setText(" ");
+        double trackingInPixels = (sceneText.getLayoutBounds().getHeight() / font.getSize()) * tracking;
+
+        double[] symbolsWidth = new double[text.length()];
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+
+            if (c == ' ') {
+                symbolsWidth[i-1] -= trackingInPixels;
+                continue;
+            }
+
+            sceneText.setText(String.valueOf(c));
+            symbolsWidth[i] = sceneText.getLayoutBounds().getWidth() + trackingInPixels;
+
+            if (i == text.length() - 1) {
+                symbolsWidth[i] -= trackingInPixels;
+            }
+        }
+
+        double textWidthInAngles = Arrays.stream(symbolsWidth).sum() / angleInPixels;
+        double emptyAreaInAngles = angleArea - textWidthInAngles;
+        double spaceWidthInAngles = emptyAreaInAngles / spaceCount;
+
+        gc.setFont(font);
+        gc.setFill(paint);
+
+        double currentAngle = startAngle;
+
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+
+            if (c == ' ') {
+                currentAngle += spaceWidthInAngles;
+                continue;
+            }
+
+            double x = getPointAtAngle(radius, currentAngle)[0];
+            double y = getPointAtAngle(radius, currentAngle)[1];
+
+            Rotate r = new Rotate(currentAngle + 90, x, y);
+            gc.setTransform(r.getMxx(), r.getMyx(), r.getMxy(), r.getMyy(), r.getTx(), r.getTy());
+
+            gc.fillText(String.valueOf(c), x, y);
+            currentAngle += symbolsWidth[i] / angleInPixels;
+        }
+
+        gc.setFont(Font.getDefault());
+        gc.setFill(Color.BLACK);
+        gc.setTransform(new Affine());
+    }
+
+    protected double[] getPointAtAngle(double radius, double angle) {
+        double centerX = canvas.getWidth() / 2;
+        double centerY = canvas.getHeight() / 2;
+
+        double x = radius * Math.cos(Math.toRadians(angle));
+        double y = radius * Math.sin(Math.toRadians(angle));
+
+        x += centerX;
+        y += centerY;
+
+        return new double[]{x, y};
+    }
+}
